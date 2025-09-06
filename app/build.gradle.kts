63bf01ae55c5d9230a7dcb115f30978ae2bb055f
Tbonis <theodorebonis@gmail.com>
2025-09-06 22:42:56 +0200

    [Typography] Added Google font family

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/io/github/theodorosB/therapath/ui/theme/Type.kt
+++ app/src/main/java/io/github/theodorosB/therapath/ui/theme/Type.kt
@@ -4,51 +4,67 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import io.github.theodorosB.therapath.R
 
+val fontProvider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+val googleFont = GoogleFont("Libertinus Serif Display")
+
+val therapathFontFamily = FontFamily(
+    Font(
+        googleFont = googleFont,
+        fontProvider = fontProvider
+    )
+)
 val Typography = Typography(
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp,
     ),
     titleMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
     ),
     titleSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
     ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 20.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 18.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
     ),
     labelMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = therapathFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
     ),
