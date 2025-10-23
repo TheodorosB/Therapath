2ea94406797801764f296f9978ebec87022c78d9
Tbonis <theodorebonis@gmail.com>
2025-10-23 16:48:53 +0200

    [Login] Updated Sign In Screen

--- app/src/main/java/io/github/theodorosB/therapath/ui/login/screens/signin/composable/SignInScreen.kt
+++ app/src/main/java/io/github/theodorosB/therapath/ui/login/screens/signin/composable/SignInScreen.kt
@@ -1,41 +1,16 @@
 package io.github.theodorosB.therapath.ui.login.screens.signin.composable
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.github.theodorosB.therapath.R
-import io.github.theodorosB.therapath.ui.app.model.FieldUiItem
-import io.github.theodorosB.therapath.ui.login.screens.signin.model.SignInUiState
-import io.github.theodorosB.therapath.ui.login.screens.signin.viewmodel.SignInViewModel
+import io.github.theodorosB.therapath.ui.login.composable.LoginScreen
+import io.github.theodorosB.therapath.ui.login.model.LoginUiState
 
 @Composable
-fun SignInScreen() {
-
-    val viewModel: SignInViewModel = hiltViewModel()
-    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+fun SignInScreen(
+    uiState: State<LoginUiState>
+) {
 
     SignInContent(
         uiState = uiState
@@ -44,79 +19,16 @@ fun SignInScreen() {
 
 @Composable
 fun SignInContent(
-    uiState: State<SignInUiState>
+    uiState: State<LoginUiState>
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(
-            space = 36.dp,
-            alignment = Alignment.CenterVertically
-        )
-    ) {
-        Text(
-            text = stringResource(id = R.string.login_title),
-            modifier = Modifier,
-            style = MaterialTheme.typography.displayLarge,
-            textAlign = TextAlign.Center,
-        )
-        Column(
-            modifier = Modifier,
-            verticalArrangement = Arrangement.spacedBy(
-                space = 24.dp,
-                alignment = Alignment.CenterVertically
-            ),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-        }
-
-    }
-
-}
-
-@Composable
-private fun FormDialogFields(
-    fields: List<FieldUiItem>
-) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(
-            space = 8.dp,
-            alignment = Alignment.CenterVertically
-        )
-    ) {
-        items(items = fields) { field ->
-            TextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(6f),
-                value = field.text.value,
-                onValueChange = { field.onUpdateText(it) },
-                label = {
-                    Text(
-                        text = stringResource(id = field.label),
-                        fontSize = 16.sp
-                    )
-                },
-                textStyle = TextStyle(
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.SemiBold
-                ),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = field.keyboardType,
-                    imeAction = ImeAction.Done
-                ),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.White,
-                    unfocusedContainerColor = Color.White,
-                    disabledContainerColor = Color.White,
-                    errorContainerColor = Color.White
-                )
-            )
-        }
-    }
+    LoginScreen(
+        fields = uiState.value.fields,
+        title = stringResource(id = uiState.value.title.value),
+        navTitle = stringResource(id = R.string.sign_up_title),
+        navDescription = stringResource(id = R.string.login_dont_have_account),
+        canResetPassword = uiState.value.canResetPassword,
+        onSubmitClicked = uiState.value.onSubmitClicked,
+        submitButtonText = stringResource(id = R. string.login_sign_in_title),
+        onForgotPasswordClicked = uiState.value.onNavigateToResetPasswordScreen
+    )
 }
